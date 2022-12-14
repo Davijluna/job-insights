0a505414320e21cec436b05d726c8a45dcd540f8
davi <davijluna@gmail.com>
2022-12-13 23:47:01 -0300

    test: requisito 10 implementando teste caso de erro

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,3 +4,5 @@ from src.pre_built.counter import count_ocurrences
 def test_counter():
     assert count_ocurrences("data/jobs.csv", "python") == 1639
     assert count_ocurrences("data/jobs.csv", "javascript") == 122
+    assert count_ocurrences("data/jobs.csv", "javascripErro") == 0
+    assert count_ocurrences("data/jobs.csv", "pythonErro") == 0
